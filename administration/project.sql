2aab1caf06a4d09e91559fde2aaadb16667f0207
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-04 20:33:49 +0100

    Update project.sql

--- administration/project.sql
+++ administration/project.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 21 déc. 2020 à 13:14
--- Version du serveur :  5.7.19
--- Version de PHP :  7.1.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- `db_admin`
---
-
--- --------------------------------------------------------
-
---
--- table `role`
---
 
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
@@ -35,19 +10,9 @@ CREATE TABLE IF NOT EXISTS `role` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
---
---  table `role`
---
-
 INSERT INTO `role` (`id`, `role`) VALUES
 (1, 'Admin'),
 (2, 'User');
-
--- --------------------------------------------------------
-
---
--- table `user`
---
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
@@ -60,14 +25,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
 
---
--- table `tbl_users`
---
-
 INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `roleid`, `isActive`) VALUES
 (1, 'admin', 'admin', 'admin@admin.admin', 1, 0),
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
