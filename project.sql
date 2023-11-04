72c328bcfbf3547a6470704f4fa29dfb0207925e
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-04 20:56:50 +0100

    Update project.sql

--- project.sql
+++ project.sql
@@ -3,28 +3,29 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE IF NOT EXISTS `role` (
+DROP TABLE IF EXISTS 'role';
+CREATE TABLE IF NOT EXISTS 'role' (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'role_id',
   `role` varchar(255) DEFAULT NULL COMMENT 'role_text',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `role` (`id`, `role`) VALUES
+INSERT INTO 'role' ('id', 'role') VALUES
 (1, 'Admin'),
 (2, 'User');
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `roleid` tinyint(4) DEFAULT NULL,
-  `isActive` tinyint(4) DEFAULT '0',
-  PRIMARY KEY (`user_id`)
+DROP TABLE IF EXISTS 'user';
+CREATE TABLE IF NOT EXISTS 'user' (
+  'user_id' int(11) NOT NULL AUTO_INCREMENT,
+  'username' varchar(255) DEFAULT NULL,
+  'password' varchar(255) DEFAULT NULL,
+  'email' varchar(255) DEFAULT NULL,
+  'avatar_id' varchar(255) DEFAULT NULL,
+  'role_id' tinyint(4) DEFAULT NULL,
+  'isActive' tinyint(4) DEFAULT '0',
+  PRIMARY KEY ('user_id')
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `roleid`, `isActive`) VALUES
-(1, 'admin', 'admin', 'admin@admin.admin', 1, 0),
+INSERT INTO 'user' ('user_id', 'username', 'password', 'email', 'avatar_id', 'role_id', 'isActive') VALUES
+(1, 'admin', 'admin', 'admin@admin.admin', NULL, 1, 0),
 COMMIT;
